1308b3eda2931dec879552465b14be9eaf4ca83b
Marcelo Glezer <marcelo.glezer@gmail.com>
2022-12-02 00:07:04 +0100

    code for day 1

---
+++ day01/calories.py
@@ -0,0 +1,17 @@
+file1 = open('aoc_input.txt', 'r')
+Lines = file1.readlines()
+
+max_cal = 0
+cals = 0
+for line in Lines:
+  line = line.strip()
+  if len(line) == 0:
+    if cals > max_cal:
+      max_cal = cals
+    cals = 0
+  else:
+    cals += int(line)
+if cals > max_cal:
+  max_cal = cals
+
+print("Max calories: ", max_cal)
---
+++ day01/top3.py
@@ -0,0 +1,25 @@
+file1 = open('aoc_input.txt', 'r')
+Lines = file1.readlines()
+
+def proc_cal(curr_maxs, calories):
+  pos = 3
+  for i in [2, 1, 0]:
+    if curr_maxs[i] > calories:
+      break
+    pos = i
+  if pos < 3:
+    curr_maxs = curr_maxs[:pos] + [calories] + curr_maxs[pos:]
+    curr_maxs = curr_maxs[:3]
+  return curr_maxs
+
+cals = 0
+top3 = [0, 0, 0]
+for line in Lines:
+  line = line.strip()
+  if len(line) == 0:
+    top3 = proc_cal(top3, cals)
+    cals = 0
+  else:
+    cals += int(line)
+top3 = proc_cal(top3, cals)
+print("Max calories: ", top3, sum(top3))
