d7057e26d4f0c0367c9b97e3479042281e7ed855
Marcelo Glezer <marcelo.glezer@gmail.com>
2022-12-02 09:45:40 +0100

    day2 part 1

---
+++ day02/rps.py
@@ -0,0 +1,28 @@
+file1 = open('aoc_day2.txt', 'r')
+lines = file1.readlines()
+
+decode = { 'A' : 'Rock', 'B': 'Paper', 'C': 'Scissors', 'X' : 'Rock', 'Y': 'Paper', 'Z': 'Scissors' }
+shape_points = { 'Rock': 1, 'Paper': 2, 'Scissors': 3 }
+match_points = { 'Lost': 0, 'Draw': 3, 'Win': 6 }
+
+def match_result(a, b):
+  if a == b:
+    return 'Draw'
+  if b == 'Rock':
+    return 'Win' if a == 'Scissors' else 'Lost'
+  if b == 'Paper':
+    return 'Win' if a == 'Rock' else 'Lost'
+  return 'Win' if a == 'Paper' else 'Lost'
+
+total_score = 0
+for line in lines:
+  line = line.strip()
+  parts = line.split(" ")
+  [oponent, myself] = map( lambda x : decode[x], parts)
+  result = match_result(oponent, myself)
+  score = shape_points[myself] + match_points[result]
+  total_score += score
+  print(oponent, myself, result, score)
+
+print(total_score)
+
