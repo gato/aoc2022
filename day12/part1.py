d0146701de9b20adf809e9b77d69337cf824e9ad
Marcelo Glezer <marcelo.glezer@gmail.com>
2022-12-12 21:32:58 +0100

    day12 compact edition

--- day12/part1.py
+++ day12/part1.py
@@ -1,40 +1,38 @@
 f = open('input.txt', 'r')
-matrix = list(map(lambda x: x.strip(), f.readlines()))
+matrix = list(map(lambda x: [ord(c) for c in x.strip()], f.readlines()))
 w = len(matrix[0])
 h = len(matrix)
 start, end = (-1,-1), (-1, -1)
 for i in range(h):
   for j in range(w):
-    if matrix[i][j] == 'E':
+    if matrix[i][j] == ord('E'):
       end = (j, i)
-      matrix[i] = matrix[i][0:j] + "z" + matrix[i][j+1:]
-    elif matrix[i][j] == 'S':
+      matrix[i][j] = ord('z')
+    elif matrix[i][j] == ord('S'):
       start = (j, i)
-      matrix[i] = matrix[i][0:j] + "a" + matrix[i][j+1:]
+      matrix[i][j] = ord('a')
 
 queue = []
 queued = set()
 queue.append((start[0], start[1], 0))
+
+def proc_node(x, y, dist, value):
+  if x < 0 or x >= w or y < 0 or y >= h:
+    return
+  if matrix[y][x] > value + 1:
+    return
+  if (x, y) in queued:
+    return
+  queue.append((x, y, dist + 1))
+  queued.add((x, y))
 
 while len(queue) > 0:
   node = queue.pop(0)
   if (node[0], node[1]) == end:
     print("Answer is", node[2])
     break 
-  value = ord(matrix[node[1]][node[0]])
-  if node[0] > 0 and ord(matrix[node[1]][node[0]-1]) <= value + 1:
-    if not (node[0]-1, node[1]) in queued:
-      queue.append((node[0]-1, node[1], node[2] + 1))
-      queued.add((node[0]-1, node[1]) )
-  if node[1] < h - 1 and ord(matrix[node[1]+1][node[0]]) <= value + 1:
-    if not (node[0], node[1]+1) in queued:
-      queue.append((node[0], node[1]+1, node[2] + 1))
-      queued.add((node[0], node[1]+1))
-  if node[0] < w - 1 and ord(matrix[node[1]][node[0]+1]) <= value + 1:
-    if not (node[0]+1, node[1]) in queued:
-      queue.append((node[0]+1, node[1], node[2] + 1))
-      queued.add((node[0]+1, node[1]))
-  if node[1] > 0 and ord(matrix[node[1]-1][node[0]]) <= value + 1:
-    if not (node[0], node[1]-1) in queued:
-      queue.append((node[0], node[1]-1, node[2] + 1))
-      queued.add((node[0], node[1]-1))
+  value = matrix[node[1]][node[0]]
+  proc_node(node[0] -1, node[1]   , node[2], value)
+  proc_node(node[0]   , node[1] +1, node[2], value)
+  proc_node(node[0] +1, node[1]   , node[2], value)
+  proc_node(node[0]   , node[1] -1, node[2], value)
--- day12/part2.py
+++ day12/part2.py
@@ -1,48 +1,46 @@
 f = open('input.txt', 'r')
-matrix = list(map(lambda x: x.strip(), f.readlines()))
+matrix = list(map(lambda x: [ord(c) for c in x.strip()], f.readlines()))
 w = len(matrix[0])
 h = len(matrix)
 end = (-1,-1)
 starts = []
 for i in range(h):
   for j in range(w):
-    if matrix[i][j] == 'E':
+    if matrix[i][j] == ord('E'):
       end = (j, i)
-      matrix[i] = matrix[i][0:j] + "z" + matrix[i][j+1:]
-    elif matrix[i][j] == 'S':
-      matrix[i] = matrix[i][0:j] + "a" + matrix[i][j+1:]
-    if matrix[i][j] == 'a':
+      matrix[i][j] = ord('z')
+    elif matrix[i][j] == ord('S'):
+      matrix[i][j] = ord('a')
+    if matrix[i][j] == ord('a'):
       starts.append((j, i))
 
-shortest = 10000000
 def shortest_path(start, end):
   queue = []
   queued = set()
   queue.append((start[0], start[1], 0))
 
+  def proc_node(x, y, dist, value):
+    if x < 0 or x >= w or y < 0 or y >= h:
+      return
+    if matrix[y][x] > value + 1:
+      return
+    if (x, y) in queued:
+      return
+    queue.append((x, y, dist + 1))
+    queued.add((x, y))
+
   while len(queue) > 0:
     node = queue.pop(0)
     if (node[0], node[1]) == end:
       return node[2]
-    value = ord(matrix[node[1]][node[0]])
-    if node[0] > 0 and ord(matrix[node[1]][node[0]-1]) <= value + 1:
-      if not (node[0]-1, node[1]) in queued:
-        queue.append((node[0]-1, node[1], node[2] + 1))
-        queued.add((node[0]-1, node[1]) )
-    if node[1] < h - 1 and ord(matrix[node[1]+1][node[0]]) <= value + 1:
-      if not (node[0], node[1]+1) in queued:
-        queue.append((node[0], node[1]+1, node[2] + 1))
-        queued.add((node[0], node[1]+1))
-    if node[0] < w - 1 and ord(matrix[node[1]][node[0]+1]) <= value + 1:
-      if not (node[0]+1, node[1]) in queued:
-        queue.append((node[0]+1, node[1], node[2] + 1))
-        queued.add((node[0]+1, node[1]))
-    if node[1] > 0 and ord(matrix[node[1]-1][node[0]]) <= value + 1:
-      if not (node[0], node[1]-1) in queued:
-        queue.append((node[0], node[1]-1, node[2] + 1))
-        queued.add((node[0], node[1]-1))
+    value = matrix[node[1]][node[0]]
+    proc_node(node[0] -1, node[1]   , node[2], value)
+    proc_node(node[0]   , node[1] +1, node[2], value)
+    proc_node(node[0] +1, node[1]   , node[2], value)
+    proc_node(node[0]   , node[1] -1, node[2], value)
   return None
 
+shortest = 10000000
 for start in starts:
   curr = shortest_path(start, end)
   if curr != None:
